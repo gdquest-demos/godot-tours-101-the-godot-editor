5805376708ff5d2e75dfa51fc9be11a4779a0775
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2024-01-05 13:29:40 +0100

    content: make game over screen look nicer

--- game_over.gd
+++ game_over.gd
@@ -1,10 +1,14 @@
 extends Control
 
-@export var restart_scene_path := "res://start.tscn"
+@onready var restart_button: Button = %RestartButton
+
+
+func _ready() -> void:
+	restart_button.grab_focus()
 
 
 func _on_RestartButton_pressed() -> void:
-	get_tree().change_scene_to_file(restart_scene_path)
+	get_tree().change_scene_to_file("res://completed_project.tscn")
 
 
 func _on_QuitButton_pressed() -> void:
