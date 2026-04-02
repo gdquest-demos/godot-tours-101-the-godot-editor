85edfdf8267bd1d15ada5227734c5a099a5b1494
Yuri Sizov <yuris@humnom.net>
2026-04-02 16:18:27 +0200

    WIP: Update series metadata, add new bookend steps

--- godot_tours.gd
+++ godot_tours.gd
@@ -3,18 +3,23 @@ extends "res://addons/godot_tours/gdtour_metadata.gd"
 
 
 func _init() -> void:
-	open_welcome_menu_automatically = true
+	auto_open_tour_list = true
+	sample_mode = true
+
+	title = "Welcome to GDTour!"
+	subtitle = "Getting Started with Godot"
+	description.push_back("TOUR SERIES DESCRIPTION")
+
 	register_tour(
 		"getting_started_101_the_godot_editor",
-		"101: The Godot Editor",
+		"101. The Godot Editor",
+		32,
 		"res://tours/godot-first-tour/godot_first_tour.gd",
-		true,
-		false
 	)
 	register_tour(
 		"getting_started_102_assemble_your_first_game",
-		"102: Assemble Your First Game",
+		"102. Assemble Your First Game",
+		99,
 		"res://tours/learn-gamedev-from-zero/assemble_path_of_sorcerers.gd",
-		false,
 		true
 	)
